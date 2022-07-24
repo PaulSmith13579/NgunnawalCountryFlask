8596e629e99ef129be1559d92764723bd223bbdf
0717097 <0717097@scholsnet.act.edu.au>
2022-07-25 09:21:38 +1000

    init

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config.from_object(Config)  # loads the configuration for the database
+db = SQLAlchemy(app)            # creates the db object using the configuration
 
 @app.route('/')
 def hello_world():  # put application's code here
