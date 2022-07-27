1ba3057cb5daa95e8419fec2b93bd46b27e42ec6
0717097 <0717097@scholsnet.act.edu.au>
2022-07-27 12:54:22 +1000

    init

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
 
@@ -7,9 +8,8 @@ app.config.from_object(Config)  # loads the configuration for the database
 db = SQLAlchemy(app)            # creates the db object using the configuration
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+def Homepage():  # put application's code here
+    return render_template("index.html", title="Ngunnawal Country")
 
 if __name__ == '__main__':
     app.run()
