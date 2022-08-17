3fe0ddea4459da8f0fca517488c52e6af4d89859
0717097 <0717097@scholsnet.act.edu.au>
2022-08-17 12:38:56 +1000

    Innit

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from app import db
 from datetime import datetime
-
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -19,3 +20,23 @@ class todo (db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     text = db.Column(db.Text)
     done = db.Column(db.Boolean)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email_address = db.Column(db.String(255), unique=True)
+    name = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
+    user_level = db.Column(db.Integer)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def is_admin(self):
+        if self.user_level == 2:
+            return True
+        else:
+            return False
